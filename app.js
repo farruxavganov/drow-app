45b2b92f8a4500450bc28cd17517ef8c315c5422
farrux <farrux.avganov@mail.ru>
2022-07-15 19:18:00 +0300

    finnished project

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const canvas = document.getElementById("canvas");
+const dicr = document.querySelector(".dicr");
+const incr = document.querySelector(".incr");
+const sizeNumber = document.querySelector(".count");
+const color = document.querySelector(".color");
+const clear = document.querySelector(".clear");
 
 let ctx = canvas.getContext("2d");
 
 let canvasSize = 15;
-let canvasColor = "red";
+let canvasColor = "#000";
 
 let isTrue = false;
 let x = undefined;
 let y = undefined;
 
+sizeShow();
+canvas.addEventListener("pointerdown", drowStart);
+canvas.addEventListener("pointermove", drowMove);
+canvas.addEventListener("pointerup", drowEnd);
+incr.addEventListener("click", sizeIncr);
+dicr.addEventListener("click", sizeDicr)
+color.addEventListener("change", changeColor);
+clear.addEventListener("click", clearWindow);
 
-
-canvas.addEventListener("pointerdown", (e)=> {
+function drowStart(e) {
 	isTrue = true;
 	x = e.offsetX;
 	y = e.offsetY;
 	drowCircle(x,y);
+}
 
-})
-
-
-canvas.addEventListener("pointermove", (e)=> {
+function drowMove(e) {
 	if(isTrue){
 		let x1 = e.offsetX;
 		let y1 = e.offsetY;
@@ -29,14 +39,46 @@ canvas.addEventListener("pointermove", (e)=> {
 		x = x1;
 		y = y1;
 	}
-})
+}
 
-canvas.addEventListener("pointerup",()=> {
+function drowEnd() {
 	isTrue = false;
 
 	x = undefined;
 	y = undefined;
-})
+}
+
+function sizeIncr() {
+	canvasSize++;
+
+	if(canvasSize > 50){
+		canvasSize = 50;
+	}
+
+	sizeShow();
+}
+
+function sizeDicr() {
+	canvasSize--;
+
+	if(canvasSize < 0) {
+		canvasSize = 1;
+	}
+
+	sizeShow();
+}
+
+function sizeShow() {
+	sizeNumber.innerHTML = canvasSize;
+}
+
+function changeColor(e){
+	canvasColor = e.currentTarget.value;
+}
+
+function clearWindow() {
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+}
 
 function drowLine(x1, y1, x2, y2) {
 	ctx.beginPath();
